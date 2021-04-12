4ae6c27099152f815a386cd40f6374d284e89231
Sathish Gopal(UST <U20931@ust-global.com>
2021-04-12 13:13:21 +0530

    Initial commit

---
+++ src/test/java/SQLServerTestContainer.java
@@ -0,0 +1,49 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.Test;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MSSQLServerContainer;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+public class SQLServerTestContainer {
+
+    String MSSQL_SERVER_IMAGE = "mcr.microsoft.com/mssql/server:2017-CU12";
+
+    @Test
+    public void testSimple() throws SQLException {
+        MSSQLServerContainer mssqlServer = new MSSQLServerContainer(MSSQL_SERVER_IMAGE);
+
+        mssqlServer.start();
+
+        ResultSet resultSet = performQuery(mssqlServer, "SELECT 1");
+
+        int resultSetInt = resultSet.getInt(1);
+        assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
+    }
+
+    protected ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql) throws SQLException {
+        DataSource ds = getDataSource(container);
+        Statement statement = ds.getConnection().createStatement();
+        statement.execute(sql);
+        ResultSet resultSet = statement.getResultSet();
+
+        resultSet.next();
+        return resultSet;
+    }
+
+    protected DataSource getDataSource(JdbcDatabaseContainer<?> container) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(container.getJdbcUrl());
+        hikariConfig.setUsername(container.getUsername());
+        hikariConfig.setPassword(container.getPassword());
+        hikariConfig.setDriverClassName(container.getDriverClassName());
+
+        return new HikariDataSource(hikariConfig);
+    }
+}
